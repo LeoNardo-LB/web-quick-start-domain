5c75e6804e3baa51fa141333461bcf77127355d5
Leonardo <tab206388@aliyun.com>
2025-12-30 03:53:33 +0800

    feat(repository): 添加日志表实体和映射接口
    - 新增 LogDO 实体类，包含业务类型、方法名、参数、结果等字段
    - 实现日志记录的开始时间和结束时间字段
    - 添加日志异常信息和线程名称字段
    - 使用 Lombok 注解简化实体代码
    - 继承 BaseDO 实现基础属性继承
    - 新增 LogMapper 接口，继承 BaseMapper 提供 CRUD 功能

---
+++ infrastructure/src/main/java/org/smm/archetype/repository/mapper/LogMapper.java
@@ -0,0 +1,14 @@
+package org.smm.archetype.repository.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import org.smm.archetype.repository.entity.LogDO;
+
+/**
+ * 日志表 映射层。
+ *
+ * @author Administrator
+ * @since 2025-12-30
+ */
+public interface LogMapper extends BaseMapper<LogDO> {
+
+}
